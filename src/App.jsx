448f4a78f5cb140f555d22ba360f111c8ddd52c1
Paulina <paulinazajac.mail@gmail.com>
2024-02-05 22:56:45 +0100

    starting project with basic ui

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Sidebar from './components/Sidebar.jsx';
+import NewProject from './components/NewProject.jsx';
+
+function App() {
+  return (
+    <>
+      <main className="h-screen my-8 flex gap-8">
+        <Sidebar />
+        <NewProject />
+      </main>
+    </>
+  );
+}
+
+export default App;
