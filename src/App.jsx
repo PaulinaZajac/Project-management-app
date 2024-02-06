abadc739a9bd88a12025f555527d1f108d984a79
Paulina <paulinazajac.mail@gmail.com>
2024-02-06 20:44:15 +0100

    added state to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
 import Sidebar from './components/Sidebar.jsx';
 import NewProject from './components/NewProject.jsx';
-
+import NoProjectSelected from './components/NoProjectSelected.jsx';
+import { useState } from 'react';
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedState: undefined,
+    projects: [],
+  });
+
+  const handleCreateProject = () => {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedState: null,
+      };
+    });
+  };
+
+  let content;
+
+  if (projectState.selectedState === null) {
+    content = <NewProject />;
+  } else if (projectState.selectedState === undefined) {
+    content = <NoProjectSelected onCreateProject={handleCreateProject} />;
+  }
+
   return (
     <>
       <main className="h-screen my-8 flex gap-8">
-        <Sidebar />
-        <NewProject />
+        <Sidebar onCreateProject={handleCreateProject} />
+        {content}
       </main>
     </>
   );
