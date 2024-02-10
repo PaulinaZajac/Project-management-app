d903aa29ff1e46b0510f85cf76ef13f37b7a7b88
Paulina <paulinazajac.mail@gmail.com>
2024-02-10 12:50:31 +0100

    final funnctionality added

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,21 @@ import Sidebar from './components/Sidebar.jsx';
 import NewProject from './components/NewProject.jsx';
 import NoProjectSelected from './components/NoProjectSelected.jsx';
 import { useState } from 'react';
+import SelectedProject from './components/SelectedProject.jsx';
 function App() {
   const [projectState, setProjectState] = useState({
     selectedState: undefined,
     projects: [],
   });
+
+  const handleOnSelectedProject = (id) => {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedState: id,
+      };
+    });
+  };
 
   const handleCreateProject = () => {
     setProjectState((prevState) => {
@@ -17,10 +27,43 @@ function App() {
     });
   };
 
-  let content;
+  const handleCancelCreateProject = () => {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedState: undefined,
+      };
+    });
+  };
+
+  const handleAddProject = (projectsData) => {
+    setProjectState((prevState) => {
+      const newProject = {
+        ...projectsData,
+        id: Math.random(),
+      };
+
+      return {
+        ...prevState,
+        selectedState: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  };
+
+  const selectedProject = projectState.projects.find(
+    (project) => project.id === projectState.selectedState
+  );
+
+  let content = <SelectedProject project={selectedProject} />;
 
   if (projectState.selectedState === null) {
-    content = <NewProject />;
+    content = (
+      <NewProject
+        onAdd={handleAddProject}
+        onCancel={handleCancelCreateProject}
+      />
+    );
   } else if (projectState.selectedState === undefined) {
     content = <NoProjectSelected onCreateProject={handleCreateProject} />;
   }
@@ -28,11 +71,14 @@ function App() {
   return (
     <>
       <main className="h-screen my-8 flex gap-8">
-        <Sidebar onCreateProject={handleCreateProject} />
+        <Sidebar
+          onCreateProject={handleCreateProject}
+          projects={projectState.projects}
+          onSelected={handleOnSelectedProject}
+        />
         {content}
       </main>
     </>
   );
 }
-
 export default App;
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,26 +1,68 @@
 import Input from './Input.jsx';
+import { useRef } from 'react';
+import Modal from './Modal.jsx';
 
-const NewProject = () => {
+const NewProject = ({ onAdd, onCancel }) => {
+  const modal = useRef();
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  const handleSave = () => {
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+
+    if (
+      enteredTitle.trim() === '' ||
+      enteredDescription.trim() === '' ||
+      enteredDueDate.trim() === ''
+    ) {
+      modal.current.open();
+      return;
+    }
+
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate,
+    });
+  };
+
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input label="Title" />
-        <Input label="Description" textarea />
-        <Input label="Due date" />
+    <>
+      <Modal ref={modal}>
+        <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+        <p className="text-stone-600 mb-4">
+          Oops. looks like your provided an invalid data
+        </p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button
+              onClick={onCancel}
+              className="text-stone-800 hover:text-stone-950"
+            >
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={handleSave}
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input ref={title} label="Title" />
+          <Input ref={description} label="Description" textarea />
+          <Input ref={dueDate} label="Due date" type="date" />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
